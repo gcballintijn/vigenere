08c4cc2ecbca6a4b2f41fd9704f57294e0372a94
Gerco Ballintijn <gerco@ballintijn.com>
2022-04-06 00:36:00 +0200

    Added the basic case forcing code, but added no unit tests (#4).

--- src/vigenere/iterator.rs
+++ src/vigenere/iterator.rs
@@ -3,11 +3,19 @@ enum VigenereMode {
     Decrypt,
 }
 
+#[derive(PartialEq)]
+pub enum ForceCase {
+    Keep,
+    ToLower,
+    ToUpper,
+}
+
 pub struct VigenereIterator<I>
     where I: Iterator<Item = char>
 {
     mode: VigenereMode,
     distances: Vec<u8>,
+    force_case: ForceCase,
     index: usize,
     iter: I,
 }
@@ -15,17 +23,27 @@ pub struct VigenereIterator<I>
 impl<I> VigenereIterator<I>
     where I: Iterator<Item = char>
 {
-    const BASE: u8 = 'A' as u8;
+    const UPPER_BASE: u8 = 'A' as u8;
+    const LOWER_BASE: u8 = 'a' as u8;
 
     fn new(mode: VigenereMode, key: &str, iter: I) -> Self {
         let distances = key.chars()
-            .map(|ch| ch as u8 - Self::BASE)
+            .map(|ch| ch.to_uppercase().next().unwrap())
+            .map(|ch| ch as u8 - Self::UPPER_BASE)
             .collect::<Vec<u8>>();
         Self {
             mode,
             distances,
+            force_case: ForceCase::Keep,
             index: 0,
             iter,
+        }
+    }
+
+    fn with_force_case(self, force_case: ForceCase) -> Self {
+        Self {
+            force_case,
+            ..self
         }
     }
 }
@@ -40,13 +58,37 @@ impl<I> Iterator for VigenereIterator<I>
             Some(ch) if ch >= 'A' && ch <= 'Z' => {
                 let distance = self.distances[self.index];
                 self.index = (self.index + 1) % self.distances.len();
-                let input = ch as u8 - Self::BASE;
+                let input = ch as u8 - Self::UPPER_BASE;
                 let output = match self.mode {
                     VigenereMode::Encrypt => (input + distance) % 26,
                     VigenereMode::Decrypt => (26 + input - distance) % 26,
                 };
-                Some((Self::BASE + output) as char)  
+                Some(
+                    if self.force_case == ForceCase::ToLower {
+                        Self::LOWER_BASE + output  
+                    } else {
+                        Self::UPPER_BASE + output
+                    } as char
+                )
             },
+
+            Some(ch) if ch >= 'a' && ch <= 'z' => {
+                let distance = self.distances[self.index];
+                self.index = (self.index + 1) % self.distances.len();
+                let input = ch as u8 - Self::LOWER_BASE;
+                let output = match self.mode {
+                    VigenereMode::Encrypt => (input + distance) % 26,
+                    VigenereMode::Decrypt => (26 + input - distance) % 26,
+                };
+                Some(
+                    if self.force_case == ForceCase::ToUpper {
+                        Self::UPPER_BASE + output
+                    } else {
+                        Self::LOWER_BASE + output  
+                    } as char
+                )
+            },
+
             Some(ch) => Some(ch),
             None => None
         }
@@ -54,26 +96,34 @@ impl<I> Iterator for VigenereIterator<I>
 }
 
 pub trait Encrypt : Iterator<Item = char> + Sized {
-    fn encrypt(self, key: &str) -> VigenereIterator<Self>;
+    fn encrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<Self>;
 }
 
 pub trait Decrypt : Iterator<Item = char> + Sized {
-    fn decrypt(self, key: &str) -> VigenereIterator<Self>;
+    fn decrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<Self>;
 }
 
 impl<I> Encrypt for I
     where I: Iterator<Item = char>
 {
-    fn encrypt(self, key: &str) -> VigenereIterator<I> {
-        VigenereIterator::new(VigenereMode::Encrypt, key, self)
+    fn encrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<I> {
+        let vig_iter = VigenereIterator::new(VigenereMode::Encrypt, key, self);
+        match force_case {
+            Some(force_case) => vig_iter.with_force_case(force_case),
+            None => vig_iter
+        }
     }
 }
 
 impl<I> Decrypt for I
     where I: Iterator<Item = char>
 {
-    fn decrypt(self, key: &str) -> VigenereIterator<I> {
-        VigenereIterator::new(VigenereMode::Decrypt, key, self)
+    fn decrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<I> {
+        let vig_iter = VigenereIterator::new(VigenereMode::Decrypt, key, self);
+        match force_case {
+            Some(force_case) => vig_iter.with_force_case(force_case),
+            None => vig_iter
+        }
     }
 }
 
@@ -82,20 +132,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn identity_encrypt() {
+    fn identity_encrypt_upper() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("A");
+        let mut iter_out = iter_in.encrypt("A", None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
     }
 
     #[test]
-    fn minimal_encrypt() {
+    fn identity_encrypt_lower() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("B");
+        let mut iter_out = iter_in.encrypt("a", None);
+        assert_eq!(iter_out.next(), Some('H'));
+        assert_eq!(iter_out.next(), Some('I'));
+        assert_eq!(iter_out.next(), None);
+    }
+
+    #[test]
+    fn minimal_encrypt_upper() {
+        let s = "HI";
+        let iter_in = s.chars();
+        let mut iter_out = iter_in.encrypt("B", None);
+        assert_eq!(iter_out.next(), Some('I'));
+        assert_eq!(iter_out.next(), Some('J'));
+        assert_eq!(iter_out.next(), None);
+    }
+
+    #[test]
+    fn minimal_encrypt_lower() {
+        let s = "HI";
+        let iter_in = s.chars();
+        let mut iter_out = iter_in.encrypt("b", None);
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), Some('J'));
         assert_eq!(iter_out.next(), None);
@@ -105,7 +175,7 @@ mod tests {
     fn bigger_encrypt() {
         let s = "HIHI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("ABC");
+        let mut iter_out = iter_in.encrypt("ABC", None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('J'));
         assert_eq!(iter_out.next(), Some('J'));
@@ -117,7 +187,7 @@ mod tests {
     fn identity_decrypt() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("A");
+        let mut iter_out = iter_in.decrypt("A", None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -127,7 +197,7 @@ mod tests {
     fn minimal_decrypt() {
         let s = "IJ";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("B");
+        let mut iter_out = iter_in.decrypt("B", None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -137,7 +207,7 @@ mod tests {
     fn bigger_decrypt() {
         let s = "HJJI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("ABC");
+        let mut iter_out = iter_in.decrypt("ABC", None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), Some('H'));
