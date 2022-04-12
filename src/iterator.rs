01dcb9cdb67b73905e63d0c0f6c982484839c9bf
Gerco Ballintijn <57731881+gcballintijn@users.noreply.github.com>
2022-04-12 21:22:42 +0200

    Add the skip configuration option (#6, #16).
    * Fixed failing doc tests.
    * Added the skip non-letter configuration option (#6).

--- src/iterator.rs
+++ src/iterator.rs
@@ -1,3 +1,5 @@
+#![warn(missing_docs)]
+
 //! Iterator-based implementation of the Vigenère cipher.
 
 /// Type of operation of the character iterator.
@@ -16,28 +18,41 @@ pub enum ForceCase {
     /// Force the character to lower case.  
     ToLower,
     /// Force the character to upper case.
-    ToUpper,  
+    ToUpper,
 }
 
-/// Iterator, over characters, that encrypts or decrypts the character using the Vigenère cipher. 
+/// Method of dealing with non-letter characters.
+#[derive(PartialEq)]
+pub enum NonLetterMode {
+    /// Keep non-letter characters.
+    Keep,
+    /// Skip (*i.e.*, remove) non-letter characters.
+    Skip,
+}
+
+/// Iterator, over characters, that encrypts or decrypts the character using the Vigenère cipher.
 pub struct VigenereIterator<I>
-    where I: Iterator<Item = char>
+where
+    I: Iterator<Item = char>,
 {
     mode: VigenereMode,
     distances: Vec<u8>,
     force_case: ForceCase,
+    none_letter_mode: NonLetterMode,
     index: usize,
     iter: I,
 }
 
 impl<I> VigenereIterator<I>
-    where I: Iterator<Item = char>
+where
+    I: Iterator<Item = char>,
 {
     const UPPER_BASE: u8 = 'A' as u8;
     const LOWER_BASE: u8 = 'a' as u8;
 
     fn new(mode: VigenereMode, key: &str, iter: I) -> Self {
-        let distances = key.chars()
+        let distances = key
+            .chars()
             .map(|ch| ch.to_uppercase().next().unwrap())
             .map(|ch| ch as u8 - Self::UPPER_BASE)
             .collect::<Vec<u8>>();
@@ -45,6 +60,7 @@ impl<I> VigenereIterator<I>
             mode,
             distances,
             force_case: ForceCase::Keep,
+            none_letter_mode: NonLetterMode::Keep,
             index: 0,
             iter,
         }
@@ -56,88 +72,126 @@ impl<I> VigenereIterator<I>
             ..self
         }
     }
+
+    fn with_none_letter_mode(self, none_letter_mode: NonLetterMode) -> Self {
+        Self {
+            none_letter_mode,
+            ..self
+        }
+    }
 }
 
 impl<I> Iterator for VigenereIterator<I>
-    where I: Iterator<Item = char>
+where
+    I: Iterator<Item = char>,
 {
     type Item = char;
 
     fn next(&mut self) -> Option<char> {
-        match self.iter.next() {
-            Some(ch) if ch >= 'A' && ch <= 'Z' => {
-                let distance = self.distances[self.index];
-                self.index = (self.index + 1) % self.distances.len();
-                let input = ch as u8 - Self::UPPER_BASE;
-                let output = match self.mode {
-                    VigenereMode::Encrypt => (input + distance) % 26,
-                    VigenereMode::Decrypt => (26 + input - distance) % 26,
-                };
-                Some(
-                    if self.force_case == ForceCase::ToLower {
-                        Self::LOWER_BASE + output  
+        loop {
+            break match self.iter.next() {
+                Some(ch) if ch >= 'A' && ch <= 'Z' => {
+                    let distance = self.distances[self.index];
+                    self.index = (self.index + 1) % self.distances.len();
+                    let input = ch as u8 - Self::UPPER_BASE;
+                    let output = match self.mode {
+                        VigenereMode::Encrypt => (input + distance) % 26,
+                        VigenereMode::Decrypt => (26 + input - distance) % 26,
+                    };
+                    Some(if self.force_case == ForceCase::ToLower {
+                        Self::LOWER_BASE + output
                     } else {
                         Self::UPPER_BASE + output
-                    } as char
-                )
-            },
-
-            Some(ch) if ch >= 'a' && ch <= 'z' => {
-                let distance = self.distances[self.index];
-                self.index = (self.index + 1) % self.distances.len();
-                let input = ch as u8 - Self::LOWER_BASE;
-                let output = match self.mode {
-                    VigenereMode::Encrypt => (input + distance) % 26,
-                    VigenereMode::Decrypt => (26 + input - distance) % 26,
-                };
-                Some(
-                    if self.force_case == ForceCase::ToUpper {
+                    } as char)
+                }
+    
+                Some(ch) if ch >= 'a' && ch <= 'z' => {
+                    let distance = self.distances[self.index];
+                    self.index = (self.index + 1) % self.distances.len();
+                    let input = ch as u8 - Self::LOWER_BASE;
+                    let output = match self.mode {
+                        VigenereMode::Encrypt => (input + distance) % 26,
+                        VigenereMode::Decrypt => (26 + input - distance) % 26,
+                    };
+                    Some(if self.force_case == ForceCase::ToUpper {
                         Self::UPPER_BASE + output
                     } else {
-                        Self::LOWER_BASE + output  
-                    } as char
-                )
-            },
-
-            Some(ch) => Some(ch),
-            None => None
+                        Self::LOWER_BASE + output
+                    } as char)
+                }
+    
+                Some(ch) => if self.none_letter_mode == NonLetterMode::Skip {
+                    continue;
+                } else {
+                    Some(ch)
+                }
+                None => None,
+            };
         }
     }
 }
 
 /// Encryption trait for a character iterator.
-pub trait Encrypt : Iterator<Item = char> + Sized {
+pub trait Encrypt: Iterator<Item = char> + Sized {
     /// Encrypt characters using the specified key and configuration.
-    fn encrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<Self>;
+    fn encrypt(
+        self,
+        key: &str,
+        force_case: Option<ForceCase>,
+        none_letter_mode: Option<NonLetterMode>,
+    ) -> VigenereIterator<Self>;
 }
 
 /// Decryption trait for a character iterator.
-pub trait Decrypt : Iterator<Item = char> + Sized {
+pub trait Decrypt: Iterator<Item = char> + Sized {
     /// Decrypt characters using the specified key and configuration.
-    fn decrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<Self>;
+    fn decrypt(
+        self,
+        key: &str,
+        force_case: Option<ForceCase>,
+        none_letter_mode: Option<NonLetterMode>,
+    ) -> VigenereIterator<Self>;
 }
 
 impl<I> Encrypt for I
-    where I: Iterator<Item = char>
+where
+    I: Iterator<Item = char>,
 {
-    fn encrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<I> {
-        let vig_iter = VigenereIterator::new(VigenereMode::Encrypt, key, self);
-        match force_case {
-            Some(force_case) => vig_iter.with_force_case(force_case),
-            None => vig_iter
+    fn encrypt(
+        self,
+        key: &str,
+        force_case: Option<ForceCase>,
+        none_letter_mode: Option<NonLetterMode>,
+    ) -> VigenereIterator<I> {
+        let mut vig_iter = VigenereIterator::new(VigenereMode::Encrypt, key, self);
+        if let Some(force_case) = force_case {
+            vig_iter = vig_iter.with_force_case(force_case);
         }
+        if let Some(none_letter_mode) = none_letter_mode {
+            vig_iter = vig_iter.with_none_letter_mode(none_letter_mode);
+        }
+        vig_iter
     }
 }
 
 impl<I> Decrypt for I
-    where I: Iterator<Item = char>
+where
+    I: Iterator<Item = char>,
 {
-    fn decrypt(self, key: &str, force_case: Option<ForceCase>) -> VigenereIterator<I> {
-        let vig_iter = VigenereIterator::new(VigenereMode::Decrypt, key, self);
-        match force_case {
-            Some(force_case) => vig_iter.with_force_case(force_case),
-            None => vig_iter
+    fn decrypt(
+        self,
+        key: &str,
+        force_case: Option<ForceCase>,
+        none_letter_mode: Option<NonLetterMode>,
+    ) -> VigenereIterator<I> {
+        let mut vig_iter = VigenereIterator::new(VigenereMode::Decrypt, key, self);
+        if let Some(force_case) = force_case {
+            vig_iter = vig_iter.with_force_case(force_case);
         }
+        if let Some(none_letter_mode) = none_letter_mode {
+            vig_iter = vig_iter.with_none_letter_mode(none_letter_mode);
+        }
+        vig_iter
     }
 }
 
@@ -149,7 +203,7 @@ mod tests {
     fn identity_encrypt_upper_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("A", None);
+        let mut iter_out = iter_in.encrypt("A", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -159,7 +213,7 @@ mod tests {
     fn identity_encrypt_lower_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("a", None);
+        let mut iter_out = iter_in.encrypt("a", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -169,7 +223,7 @@ mod tests {
     fn identity_encrypt_upper_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("A", None);
+        let mut iter_out = iter_in.encrypt("A", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -179,7 +233,7 @@ mod tests {
     fn identity_encrypt_lower_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("a", None);
+        let mut iter_out = iter_in.encrypt("a", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -189,7 +243,7 @@ mod tests {
     fn minimal_encrypt_upper_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("B", None);
+        let mut iter_out = iter_in.encrypt("B", None, None);
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), Some('J'));
         assert_eq!(iter_out.next(), None);
@@ -199,7 +253,7 @@ mod tests {
     fn minimal_encrypt_lower_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("b", None);
+        let mut iter_out = iter_in.encrypt("b", None, None);
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), Some('J'));
         assert_eq!(iter_out.next(), None);
@@ -209,7 +263,7 @@ mod tests {
     fn minimal_encrypt_upper_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("B", None);
+        let mut iter_out = iter_in.encrypt("B", None, None);
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), Some('j'));
         assert_eq!(iter_out.next(), None);
@@ -219,7 +273,7 @@ mod tests {
     fn minimal_encrypt_lower_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("b", None);
+        let mut iter_out = iter_in.encrypt("b", None, None);
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), Some('j'));
         assert_eq!(iter_out.next(), None);
@@ -229,7 +283,7 @@ mod tests {
     fn bigger_encrypt() {
         let s = "HiHi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("ABC", None);
+        let mut iter_out = iter_in.encrypt("ABC", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('j'));
         assert_eq!(iter_out.next(), Some('J'));
@@ -241,7 +295,7 @@ mod tests {
     fn bigger_encrypt_force_lower() {
         let s = "HiHi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("ABC", Some(ForceCase::ToLower));
+        let mut iter_out = iter_in.encrypt("ABC", Some(ForceCase::ToLower), None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('j'));
         assert_eq!(iter_out.next(), Some('j'));
@@ -253,7 +307,7 @@ mod tests {
     fn bigger_encrypt_force_upper() {
         let s = "HiHi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.encrypt("ABC", Some(ForceCase::ToUpper));
+        let mut iter_out = iter_in.encrypt("ABC", Some(ForceCase::ToUpper), None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('J'));
         assert_eq!(iter_out.next(), Some('J'));
@@ -262,10 +316,22 @@ mod tests {
     }
 
     #[test]
+    fn bigger_encrypt_skip_nonletters() {
+        let s = "H-i H+i";
+        let iter_in = s.chars();
+        let mut iter_out = iter_in.encrypt("ABC", None, Some(NonLetterMode::Skip));
+        assert_eq!(iter_out.next(), Some('H'));
+        assert_eq!(iter_out.next(), Some('j'));
+        assert_eq!(iter_out.next(), Some('J'));
+        assert_eq!(iter_out.next(), Some('i'));
+        assert_eq!(iter_out.next(), None);
+    }
+
+    #[test]
     fn identity_decrypt_upper_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("A", None);
+        let mut iter_out = iter_in.decrypt("A", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -275,7 +341,7 @@ mod tests {
     fn identity_decrypt_lower_key_upper_input() {
         let s = "HI";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("a", None);
+        let mut iter_out = iter_in.decrypt("a", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -285,7 +351,7 @@ mod tests {
     fn identity_decrypt_upper_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("A", None);
+        let mut iter_out = iter_in.decrypt("A", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -295,7 +361,7 @@ mod tests {
     fn identity_decrypt_lower_key_lower_input() {
         let s = "hi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("a", None);
+        let mut iter_out = iter_in.decrypt("a", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -305,7 +371,7 @@ mod tests {
     fn minimal_decrypt_upper_key_upper_input() {
         let s = "IJ";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("B", None);
+        let mut iter_out = iter_in.decrypt("B", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -315,7 +381,7 @@ mod tests {
     fn minimal_decrypt_lower_key_upper_input() {
         let s = "IJ";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("b", None);
+        let mut iter_out = iter_in.decrypt("b", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), None);
@@ -325,7 +391,7 @@ mod tests {
     fn minimal_decrypt_upper_key_lower_input() {
         let s = "ij";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("B", None);
+        let mut iter_out = iter_in.decrypt("B", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -335,7 +401,7 @@ mod tests {
     fn minimal_decrypt_lower_key_lower_input() {
         let s = "ij";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("b", None);
+        let mut iter_out = iter_in.decrypt("b", None, None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
@@ -345,7 +411,7 @@ mod tests {
     fn bigger_decrypt() {
         let s = "HjJi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("ABC", None);
+        let mut iter_out = iter_in.decrypt("ABC", None, None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), Some('H'));
@@ -357,7 +423,7 @@ mod tests {
     fn bigger_decrypt_force_lower() {
         let s = "HjJi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("ABC", Some(ForceCase::ToLower));
+        let mut iter_out = iter_in.decrypt("ABC", Some(ForceCase::ToLower), None);
         assert_eq!(iter_out.next(), Some('h'));
         assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), Some('h'));
@@ -369,11 +435,23 @@ mod tests {
     fn bigger_decrypt_force_upper() {
         let s = "HjJi";
         let iter_in = s.chars();
-        let mut iter_out = iter_in.decrypt("ABC", Some(ForceCase::ToUpper));
+        let mut iter_out = iter_in.decrypt("ABC", Some(ForceCase::ToUpper), None);
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
         assert_eq!(iter_out.next(), Some('H'));
         assert_eq!(iter_out.next(), Some('I'));
+        assert_eq!(iter_out.next(), None);
+    }
+
+    #[test]
+    fn bigger_decrypt_skip_nonletters() {
+        let s = "H.j^J,i";
+        let iter_in = s.chars();
+        let mut iter_out = iter_in.decrypt("ABC", None, Some(NonLetterMode::Skip));
+        assert_eq!(iter_out.next(), Some('H'));
+        assert_eq!(iter_out.next(), Some('i'));
+        assert_eq!(iter_out.next(), Some('H'));
+        assert_eq!(iter_out.next(), Some('i'));
         assert_eq!(iter_out.next(), None);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,28 @@
+#![warn(missing_docs)]
+
 //! Implementation of the Vigenère cipher.
 //!
 //! For some background information, see the [Wikipedia entry](https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher).
 //!
 //! ### Encyption example
-//! ```
+//! ```rust
+//! # fn main() {
 //!     let cypher = vigenere::Vigenere::new("WHYRUST");
 //!     let cipher_text = cypher.encrypt("TO EMPOWER EVERYONE");
-//!     println!("Encrypting '{}' gives '{}'.", plain_text, cipher_text);
+//!     println!("Encrypting gives '{}'.", cipher_text);
+//! # }
 //! ```
-//! Resulting output: `Encrypting 'TO EMPOWER EVERYONE' gives 'PV CDJGPAY CMYJRKUC'.`
+//! Resulting output: `Encrypting gives 'PV CDJGPAY CMYJRKUC'.`
 //! 
 //! ### Decyption example
-//! ```
+//! ```rust
+//! # fn main() {
 //!     let cypher = vigenere::Vigenere::new("WHYRUST");
 //!     let plain_text = cypher.decrypt("PV CDJGPAY CMYJRKUC");
-//!     println!("Decrypting '{}' gives '{}'.", cipher_text, plain_text);
+//!     println!("Decrypting gives '{}'.", plain_text);
+//! # }
 //! ```
-//! Resulting output: `Decrypting 'PV CDJGPAY CMYJRKUC' gives 'TO EMPOWER EVERYONE'.`
+//! Resulting output: `Decrypting gives 'TO EMPOWER EVERYONE'.`
 
 pub mod iterator;
 
@@ -33,12 +39,12 @@ impl Vigenere {
 
     /// Encrypts the provided plain text and returns the resulting cipher text.
     pub fn encrypt(&self, plain_text: &str) -> String {
-        plain_text.chars().encrypt(&self.0, None).collect()
+        plain_text.chars().encrypt(&self.0, None, None).collect()
     }
     
     /// Decrypts the provided cipher text and returns the resulting plain text.
     pub fn decrypt(&self, cipher_text: &str) -> String {
-        cipher_text.chars().decrypt(&self.0, None).collect()
+        cipher_text.chars().decrypt(&self.0, None, None).collect()
     }    
 }
 
